ec3d35933a32ae4ce1bc2b21de1d72ceacd97094
kristiyannikolaev <krisesuper@gmail.com>
2023-08-06 15:06:19 +0300

    Create qet requested offers function

--- renteable/src/app/pages/requested-offers/requested-offers.component.ts
+++ renteable/src/app/pages/requested-offers/requested-offers.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { OffersService } from '../offers.service';
+import { Offer } from 'src/app/interfaces/Offer';
 
 @Component({
   selector: 'app-requested-offers',
   templateUrl: './requested-offers.component.html',
   styleUrls: ['./requested-offers.component.css']
 })
-export class RequestedOffersComponent {
+export class RequestedOffersComponent implements OnInit{
+
+  requestedOffers: Offer[];
+  requestedOffersSubscription$: Subscription;
+  
+  constructor(private offersService: OffersService) {}
+
+  ngOnInit(): void {
+    this.requestedOffersSubscription$ = this.offersService.getAllOffers().subscribe((offers) => {
+      this.requestedOffers = Object.values(offers).filter((x) => x.requestedBy?.includes(this.offersService.user?.uid));
+      console.log(this.requestedOffers);
+    });
+  }
 
 }
